7cc5ee4268fb32175ba759b5fd518e9a8f1e41fa
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2022-11-19 15:53:15 +0100

    Separate hero image mockups

---
+++ src/components/HeroImg.tsx
@@ -0,0 +1,8 @@
+export const HeroImg = () => {
+  return (
+    <div className="rounded-4xl bg-amber-400/70 flex gap-6 py-4 px-10">
+      <img src="/meals.png" width="200" alt="" className="-mb-8" />
+      <img src="/groceries.png" width="200" alt="" className="-mt-8" />
+    </div>
+  );
+};
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      borderRadius: {
+        "4xl": "30px",
+      },
+    },
   },
   plugins: [],
 };
