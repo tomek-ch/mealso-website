c9b73d8bf79d049f8543be30e19e52702f93c2cc
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2022-11-19 18:06:05 +0100

    Update steps section styles

--- src/components/Steps.tsx
+++ src/components/Steps.tsx
@@ -2,17 +2,9 @@ import { Container } from "./Container";
 import { Heading } from "./Heading";
 import { ArrowRight } from "./icons/ArrowRight";
 
-const Step = ({
-  img,
-  alt,
-  text,
-}: {
-  img: string;
-  alt: string;
-  text: string;
-}) => {
+const Step = ({ img = "", alt = "", text = "", className = "" }) => {
   return (
-    <div>
+    <div className={className}>
       <img src={img} alt={alt} width="200" />
       <div className="text-center mt-6">{text}</div>
     </div>
@@ -24,23 +16,24 @@ export const Steps = () => {
     <div className="py-10 bg-amber-400/50">
       <Container>
         <Heading>Simplify your meal planning</Heading>
-        <div className="flex justify-between md:content-center">
+        <div className="flex justify-between gap-4 md:content-center">
           <Step
             img="/add-meal.png"
             text="1. Add your favorite meals"
             alt="Meal form in the Mealso mobile app"
           />
-          <ArrowRight width="24" />
+          <ArrowRight width="24" className="pb-16" />
           <Step
             img="/groceries.png"
             text="2. Make a shopping list"
             alt="Shopping list in the Mealso mobile app"
           />
-          <ArrowRight width="24" />
+          <ArrowRight width="24" className="hidden md:block pb-16" />
           <Step
             img="/meals.png"
             text="3. Plan your next meal"
             alt="Meal list in the Mealso mobile app"
+            className="hidden md:block"
           />
         </div>
       </Container>
