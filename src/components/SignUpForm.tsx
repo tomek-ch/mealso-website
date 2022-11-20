09c22647d728a83ed58fba75203788f288fcc43d
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2022-11-20 20:53:30 +0100

    Handle form state

--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -1,16 +1,28 @@
+import { useState } from "react";
 import { Button } from "./Button";
 import { Input } from "./Input";
 
 interface SignUpFormProps {}
 
 export const SignUpForm = ({}: SignUpFormProps) => {
+  const [input, setInput] = useState("");
   return (
     <>
       <p className="text-slate-600">
         Sign up to get notified when Mealso is released.
       </p>
-      <form className="flex gap-2 mt-4">
-        <Input className="" autoFocus placeholder="Email" />
+      <form
+        className="flex gap-2 mt-4"
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.log(input);
+        }}
+      >
+        <Input
+          autoFocus
+          placeholder="Email"
+          onChange={({ target: { value } }) => setInput(value)}
+        />
         <Button className="ml-auto">Sign up</Button>
       </form>
     </>
